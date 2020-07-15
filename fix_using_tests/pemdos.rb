be2f2808026ed8ce6c00f2bda22860b233013b4f
Maysoon Tabali <mtab03@me.com>
2020-07-15 19:16:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,9 @@ require 'pry'
 def snake_it_up(string)
  # binding.pry
   if string[0] == "s"
-    10 * "s" + string
+    new_string = "s" + new_string
+  end 
+  new_string
   else
     string
   end
